f0bcd37d794daa3296f9b66bcd711645e229e33a
Lefey <33575324+Lefey@users.noreply.github.com>
2020-05-02 01:37:06 +1000

    Create narodmon_sender.py

---
+++ apps/narodmon_sender/narodmon_sender.py
@@ -0,0 +1,9 @@
+# AppDaemon narodmon.ru sender script.
+# Credits: @Lefey / https://github.com/Lefey/ad_narodmon_sender
+# To enable this script add to /config/appdaemon/apps/apps.yaml:
+# narodmon_sender:
+#   module: narodmon_sender
+#   class: narodmon_sender
+#   sensor_list: comma-separated home assistant sensor entities (without spaces)
+# For example:
+# sensor_list: sensor.outside_temperature,sensor.outside_humidity
